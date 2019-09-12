8717ad709541ff8eb26d375a656884a42e67caae
vuo5583 <georgevuong@northwesternmutual.com>
2019-09-12 18:41:03 -0500

    add react hook to App component

--- react-complete-guide/src/index.js
+++ react-complete-guide/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import AppHook from './AppHook';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<AppHook />, document.getElementById('root'));
 registerServiceWorker();
