52f563952e5e16a8c200ffc5513d2ff58f496827
vuo5583 <georgevuong@northwesternmutual.com>
2019-09-12 18:40:31 -0500

    set state in class based component

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -6,11 +6,26 @@ import Person from './Person/Person';
 class App extends Component {
   // state is a reserved word to manage data inside a component, whereas props are set and passed externally. 
   state = {
-    person: [
+    persons: [
       { name: 'George', age: 29 }, // pass number for age instead of string
       { name: 'Angela', age: 29 },
       { name: 'Stephanie', age: 26 }
     ]
+  }
+
+  // need to use fat arrow functions for "this" to work in handler.
+  // "this" will refer to the class App at runtime, and is only used in class-based components, not functional components.
+  switchNameHandler = () => {
+    console.log('You clicked me!')
+    // we haven't defined setState function, because we extended Component from
+    // the react library, the Component object has a setState function
+    this.setState({
+      persons: [
+        { name: 'Vuong', age: 29 }, // pass number for age instead of string
+        { name: 'Angela', age: 29}, 
+        { name: 'Stephanie', age: 5}
+      ]
+    })
   }
 
   render() {
@@ -18,7 +33,8 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working.</p>
-        <button>Switch Name</button>
+        {/* pass onClick handler function as a reference, do not invoke with ()*/}
+        <button onClick={this.switchNameHandler}>Switch Name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age}></Person>
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age}> Things in Common: From California </Person>
         < Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
