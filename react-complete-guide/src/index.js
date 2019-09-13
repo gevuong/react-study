3f691a704de74a69856c86e673170afde1f5b4e8
vuo5583 <georgevuong@northwesternmutual.com>
2019-09-12 19:31:10 -0500

    render class based App component

--- react-complete-guide/src/index.js
+++ react-complete-guide/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import AppHook from './AppHook';
+import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<AppHook />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
