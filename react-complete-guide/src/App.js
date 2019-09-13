f381f2217b3c36648b0bfb0ad5f909865325803b
vuo5583 <georgevuong@northwesternmutual.com>
2019-09-13 00:22:58 -0500

    create list of persons and delete handler, update name change handler

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -8,10 +8,11 @@ class App extends Component {
   // are set and passed externally. 
   state = {
     persons: [
-      { name: 'George', age: 29 }, // pass number for age instead of string
-      { name: 'Angela', age: 29 },
-      { name: 'Stephanie', age: 26 }
-    ]
+      { id: '1', name: 'George', age: 29 }, // pass age number instead of string
+      { id: '2', name: 'Angela', age: 29 },
+      { id: '3', name: 'Stephanie', age: 26 }
+    ],
+    showPersons: false
   }
 
   // need to use fat arrow functions for "this" to work in handler.
@@ -30,15 +31,37 @@ class App extends Component {
     })
   }
 
-  nameChangeHandler = (event) => {
-    // console.log(event.target.value)
-    this.setState({
-      persons: [
-        { name: 'George', age: 29 }, // pass number for age instead of string
-        { name: 'Angela', age: 29 }, 
-        { name: event.target.value, age: 5 }       
-      ]
-    })
+  nameChangeHandler = (event, id) => {
+    // function gets executed on every element in array
+    const personIdx = this.state.persons.findIndex(p => p.id === id )
+
+    // create a new object with all its properties using spread operator
+    const person = {...this.state.persons[personIdx]}
+    // alternative approach below
+    // const person = Object.assign({}, this.state.persons[personIndex])
+
+    person.name = event.target.value
+
+    // update one element without mutating the state
+    const persons = [...this.state.persons]
+    persons[personIdx] = person
+    this.setState({persons: persons})
+  }
+
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons
+    this.setState({ showPersons: !doesShow })
+  }
+
+  deletePersonHandler = idx => {
+    // arrays and objects are reference types. It is important to update
+    // state immutably by using slice() or the spread operator to create a 
+    // copy of the array of objects.
+    // const persons = this.state.persons.slice()
+    const persons = [...this.state.persons]
+    persons.splice(idx, 1)
+    // 
+    this.setState({persons: persons})
   }
 
   render() {
@@ -49,6 +72,26 @@ class App extends Component {
       padding: '8px',
       cursor: 'pointer'
     };
+
+    let persons = null
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          {this.state.persons.map((person, idx) => {
+            return <Person 
+              key={person.id}
+              name={person.name} 
+              age={person.age}
+              // change={this.nameChangeHandler.bind(this, person.id)}
+              change={(event) => this.nameChangeHandler(event, person.id)}
+              // both methods below work the same if you're passing in a argument
+              // click={this.deletePersonHandler.bind(this, idx)}
+              click={() => this.deletePersonHandler(idx)}
+              />
+          })}
+        </div>
+      )
+    }
 
     return (
       <div className="App">
@@ -61,23 +104,11 @@ class App extends Component {
         inefficient, use bind syntax instead.*/}
         <button
           style={style}
-          onClick={() => this.switchNameHandler('George!!')}>
-          Switch Name
+          // onClick={() => this.switchNameHandler('George!!')}>
+          onClick={this.togglePersonsHandler}>
+          Toggle Persons
         </button>
-        <Person 
-          name={this.state.persons[0].name} 
-          age={this.state.persons[0].age}
-        />
-        <Person 
-          name={this.state.persons[1].name} 
-          age={this.state.persons[1].age}
-          click={this.switchNameHandler.bind(this, 'Max')}>
-          Things in Common: From California
-        </Person>
-        <Person 
-          name={this.state.persons[2].name} 
-          age={this.state.persons[2].age}
-          change={this.nameChangeHandler}/>
+        {persons} 
       </div>
     );
   }
