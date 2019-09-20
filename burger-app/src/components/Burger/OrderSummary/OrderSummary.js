e41f5e1b11d0074eaaf7d7cddec3631ba0dfc1bc
vuo5583 <georgevuong@northwesternmutual.com>
2019-09-20 14:59:56 -0500

    add shouldComponentUpdate to Modal component for performance optimization

--- burger-app/src/components/Burger/OrderSummary/OrderSummary.js
+++ burger-app/src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,39 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-import Aux from '../../../hoc/Aux';
+import Aux from '../../../hoc/Aux/Aux';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
-    const ingredientSummary = Object.keys(props.ingredients)
-        .map(ingredientKey => {
-            // set inline styling to a JS object. The outer {} is for
-            // marking dynamic entries, and the inner {} is a JS object
-            return (
-                <li key={ingredientKey}>
-                    <span style={{textTransform: 'capitalize'}}>
-                        {ingredientKey}</span>: {props.ingredients[ingredientKey]
-                }</li>
-            );
-        })
-    return (
-        <Aux>
-            <h3>Your Order</h3>
-            <p>A delicious burger with the following ingredients:</p>
-            <ul>
-                {ingredientSummary}
-            </ul>
-            <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
-            <p>Continue to Checkout?</p>
-            <Button 
-                buttonType={"Danger"} 
-                clicked={props.purchaseCanceled}>CANCEL
-            </Button>
-            <Button 
-                buttonType={"Success"}
-                clicked={props.purchaseContinued}>CONTINUE
-            </Button>
-        </Aux>
-    )
+// Note: component was originally a functional component. Reason for 
+// change to a class is to add componentWillUpdate for debugging purposes.
+class OrderSummary extends Component {
+    UNSAFE_componentWillUpdate() {
+        console.log('OrderSummary will update')
+    }
+
+    render() {
+        const ingredientSummary = Object.keys(this.props.ingredients)
+            .map(ingredientKey => {
+                // set inline styling to a JS object. The outer {} is for
+                // marking dynamic entries, and the inner {} is a JS object
+                return (
+                    <li key={ingredientKey}>
+                        <span style={{textTransform: 'capitalize'}}>
+                            {ingredientKey}</span>: {this.props.ingredients[ingredientKey]
+                    }</li>
+                );
+            })
+
+        return (
+            <Aux>
+                <h3>Your Order</h3>
+                <p>A delicious burger with the following ingredients:</p>
+                <ul>
+                    {ingredientSummary}
+                </ul>
+                <p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
+                <p>Continue to Checkout?</p>
+                <Button 
+                    buttonType={"Danger"} 
+                    clicked={this.props.purchaseCanceled}>CANCEL
+                </Button>
+                <Button 
+                    buttonType={"Success"}
+                    clicked={this.props.purchaseContinued}>CONTINUE
+                </Button>
+            </Aux>
+        )
+    }
 }
 
-export default orderSummary
+export default OrderSummary;
--- burger-app/src/components/UI/Modal/Modal.js
+++ burger-app/src/components/UI/Modal/Modal.js
@@ -1,27 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import classes from './Modal.css';
 import Backdrop from '../Backdrop/Backdrop';
-import Aux from '../../../hoc/Aux';
+import Aux from '../../../hoc/Aux/Aux';
 
-const modal = (props) => (
-    <Aux>
-        <Backdrop 
-            show={props.show} 
-            clicked={props.modalClosed}>
-        </Backdrop>
-        <div 
-            className={classes.Modal}
-            // "vh" is a special unit referring to the viewport height, which
-            // will cause modal to slide out of the screen.
-            style={{
-                transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                opacity: props.show ? '1' : '0'
-            }}>
-            {props.children}
-        </div>
-    </Aux>
-)
+// const modal = (props) => (
+class Modal extends Component {
+    // method is used to let React know whether this component needs to be 
+    // updated. We only need to update this component if props.show changes.
+    // Otherwise, don't unnecessarily re-render Modal.
+    // Method is invoked before rendering when new props or state are being 
+    // received. This method is not called for the initial render. 
+    // Method only exists as a performance optimization.
+    shouldComponentUpdate(nextProps, _) {
+        return nextProps.show !== this.props.show
+    }
+    // method is invoked before rendering when new props or state are being
+    // received. Use this as an opportunity to perform preparation before
+    // an update occurs. This method is not called for the initial render
+    // and if shouldComponentUpdate returns false.
+    UNSAFE_componentWillUpdate() {
+        console.log('Modal Will Update')
+    }
+
+    render() {
+        return (
+            <Aux>
+                <Backdrop 
+                    show={this.props.show} 
+                    clicked={this.props.modalClosed}>
+                </Backdrop>
+                <div 
+                    className={classes.Modal}
+                    // "vh" is a special unit referring to the viewport height, which
+                    // will cause modal to slide out of the screen.
+                    style={{
+                        transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                        opacity: this.props.show ? '1' : '0'
+                    }}>
+                    {this.props.children}
+                </div>
+            </Aux>
+        )
+    }
+}
 
 
-export default modal;
+export default Modal;
