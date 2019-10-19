15ad10d208dcbb794fc8f88c5c29643a18d35298
George Vuong <vuo5583@MacBook-Pro.local>
2019-10-19 12:29:11 -0500

    create add ingredient and remove ingredient handler

--- react-hooks-practice/src/components/Ingredients/Ingredients.js
+++ react-hooks-practice/src/components/Ingredients/Ingredients.js
@@ -1,16 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import IngredientForm from './IngredientForm';
+import IngredientList from './IngredientList';
 import Search from './Search';
 
-function Ingredients() {
+const Ingredients = () => {
+  const [ userIngredients, setUserIngredients ] = useState([])
+
+  const addIngredientHandler = ingredient => {
+    setUserIngredients(prevIngredients => (
+      [...prevIngredients, {
+        id: Math.random().toString(), ...ingredient}]
+    ))
+  }
+
+  const removeIngredientHandler = ingredientId => { 
+    setUserIngredients(
+      userIngredients.filter(ingredient => ( 
+        ingredient.id !== ingredientId 
+      ))
+    )
+  }
+
   return (
     <div className="App">
-      <IngredientForm />
+      <IngredientForm onAddIngredient={addIngredientHandler} />
 
       <section>
         <Search />
-        {/* Need to add list here! */}
+        <IngredientList 
+          onRemoveItem={removeIngredientHandler}
+          ingredients={userIngredients}
+        />
       </section>
     </div>
   );
